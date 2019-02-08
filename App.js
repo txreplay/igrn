bdee043081c06442030d40c3b14294d1316e7576
txreplay <txreplay@gmail.com>
2019-02-08 16:03:59 +0100

    Camera and gallery on the same page

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import {Gallery} from './Gallery';
 import {CameraApp} from './Camera';
 
 const AppNavigator = createAppContainer(createStackNavigator({
     CameraApp: {screen: CameraApp},
-    Gallery: {screen: Gallery}
 }, {
     initialRouteName: 'CameraApp'
 }));
--- Camera.js
+++ Camera.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Camera, MediaLibrary, Permissions} from "expo";
-import {Text, TouchableOpacity, View} from "react-native";
+import {Camera, FileSystem, MediaLibrary, Permissions} from "expo";
+import {Image, Text, TouchableOpacity, View, ScrollView} from "react-native";
 
 export class CameraApp extends React.Component {
     static navigationOptions = {
@@ -10,8 +10,10 @@ export class CameraApp extends React.Component {
     state = {
         hasCameraPermission: null,
         type: Camera.Constants.Type.back,
+        ratio: '4:3',
         pictureSize: undefined,
         pictureSizes: [],
+        assets: null
     };
 
     async componentDidMount() {
@@ -20,27 +22,55 @@ export class CameraApp extends React.Component {
         this.setState({
             hasCameraPermission: cameraStatus === 'granted' && cameraRollStatus === 'granted'
         });
+
+        await this.getPictures();
     }
 
     takePicture = async () => {
         if (this.camera) {
-
             const pic = await this.camera.takePictureAsync({quality: 0.5});
             const asset = await MediaLibrary.createAssetAsync(pic.uri);
+            await MediaLibrary.createAlbumAsync('igrn', asset);
 
-            MediaLibrary.createAlbumAsync('igrn', asset);
+            await this.getPictures();
         }
     };
 
     collectPictureSizes = async () => {
         if (this.camera) {
-            const pictureSizes = await this.camera.getAvailablePictureSizesAsync('4:3');
+            const pictureSizes = await this.camera.getAvailablePictureSizesAsync(this.state.ratio);
             this.setState({ pictureSizes, pictureSize: pictureSizes[0] });
         }
     };
 
+    async getPictures() {
+        const album = await MediaLibrary.getAlbumAsync('igrn');
+        const assetsO = await MediaLibrary.getAssetsAsync({album: album});
+        const assets = assetsO.assets.reverse();
+
+        for (const i in assets) {
+            assets[i].src = await this.addSrc(assets[i]);
+        }
+
+        await this.setState({assets: assets});
+    }
+
+    async addSrc(asset) {
+        const data = await FileSystem.readAsStringAsync(asset.uri, {encoding: FileSystem.EncodingTypes.Base64});
+        return 'data:image/png;base64,' + data;
+    }
+
+
     render() {
         const {hasCameraPermission} = this.state;
+
+        let gallery = [];
+
+        for (const i in this.state.assets) {
+            gallery.push(
+                <Image key={i} source={{uri: this.state.assets[i].src}} style={{width: 120, height: 160}} />
+            );
+        }
 
         if (hasCameraPermission === null) {
             return <View/>;
@@ -50,38 +80,40 @@ export class CameraApp extends React.Component {
             return (
                 <View style={{flex: 1}}>
                     <Camera
-                        style={{flex: 1}}
+                        style={{flex: 3}}
                         type={this.state.type}
                         ref={ref => {this.camera = ref;}}
                         onCameraReady={this.collectPictureSizes}
                         pictureSize={this.state.pictureSize}
+                        ratio={this.state.ratio}
                     >
                         <View
                             style={{
                                 flex: 1,
                                 backgroundColor: 'transparent',
-                                flexDirection: 'column',
-                                justifyContent: 'center',
+                                flexDirection: 'row',
+                                justifyContent: 'space-evenly',
                                 alignItems: 'flex-end',
+                                marginBottom: 20
                             }}>
-                            <TouchableOpacity style={{flex: 0.1}}
-                                              onPress={() => {
-                                                  this.setState({
-                                                      type: this.state.type === Camera.Constants.Type.back
-                                                          ? Camera.Constants.Type.front
-                                                          : Camera.Constants.Type.back,
-                                                  });
-                                              }}>
-                                <Text style={{fontSize: 50, width: 100}}>🔄</Text>
+                            <TouchableOpacity style={{width: 60, height: 60}}
+                            onPress={() => {
+                                this.setState({
+                                    type: this.state.type === Camera.Constants.Type.back
+                                      ? Camera.Constants.Type.front
+                                      : Camera.Constants.Type.back,
+                                });
+                            }}>
+                                <Text style={{fontSize: 45}}>🔄</Text>
                             </TouchableOpacity>
-                            <TouchableOpacity onPress={this.takePicture} style={{flex: 0.1}}>
-                                <Text style={{fontSize: 50, width: 100}}>📸</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Gallery')} style={{flex: 0.1}}>
-                                <Text style={{fontSize: 50, width: 100}}>📂</Text>
+                            <TouchableOpacity onPress={this.takePicture} style={{width: 60, height: 60}}>
+                                <Text style={{fontSize: 45}}>📸</Text>
                             </TouchableOpacity>
                         </View>
                     </Camera>
+                    <ScrollView horizontal={true} style={{flex: 1}}>
+                        {gallery}
+                    </ScrollView>
                 </View>
             );
         }
